22dc6c2ab266ae2302ad4f8b37af1c177928ed37
Anjola2 <141361128+Anjola2@users.noreply.github.com>
2024-11-03 21:25:31 -0500

    Create J1_2024.py

---
+++ CCC/J1_2024.py
@@ -0,0 +1,23 @@
+#Problem Description
+#There is a new conveyor belt sushi restaurant in town. Plates of sushi travel around the restaurant on a raised conveyor belt and customers choose what to eat by removing plates.
+
+#Each red plate of sushi costs 3
+# each green plate of sushi costs 4
+#and each blue plate of sushi costs 5
+
+
+
+#Anjola Kamoru
+#Conveyor Belt Sushi
+#3 Nov 2024
+
+#input
+red=int(input(" "))
+green=int(input(""))
+blue=int(input(""))
+
+#calculating the total cost
+total=(red*3)+(green*4)+(blue*5)
+
+#output
+print(total)
